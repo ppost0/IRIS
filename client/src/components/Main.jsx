cea351dbd2a51de1cd7ac8dbbe4945771914d900
ppost0 <p.post2321@yahoo.com>
2024-03-20 17:49:09 -0400

    Create main component

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Main = () => {
+  return (
+    <div className='main-container'>
+      This is Main container.
+    </div>
+  );
+};
+
+
+export default App;
