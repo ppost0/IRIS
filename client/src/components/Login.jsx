eac3a2182f74654b7c80573abf7047ab87bd250b
ppost0 <p.post2321@yahoo.com>
2024-03-23 12:07:31 -0400

    Resolve tailwind bug with jsx files, add more styling to login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,16 +5,17 @@ const Login = () => {
   return (
 
     <div className='login-container flex flex-col items-center justify-center w-screen h-screen bg-gray-200 text-gray-700'>
-      <h1 className='login-title font-bold text-2xl'>Welcome</h1>
+      <h1 className='login-title font-bold text-2xl'>Welcome to Iris</h1>
       <form className='flex flex-col bg-white rounded shadow-lg p-12 mt-12' action=''>
-        <label htmlFor='usernameField '>Username or Email</label>
+        <label htmlFor='usernameField' className='font-semibold text-xs'>Username or Email</label>
         <input className='flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2' type='text'></input>
-        <label htmlFor='passwordField'>Password</label>
+        <label htmlFor='passwordField' className='font-semibold text-xs mt-3'>Password</label>
         <input className='flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2' type='password'></input>
-        <button>Login</button>
+        <button className='flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700'>Login</button>
         <div className='login-redirects flex mt-6 justify-center text-xs'>
-          <a href='#'>Forgot Password</a>
-          <a href='#'>Sign Up</a>
+          <a className='text-blue-400 hover:text-blue-500' href='#'>Forgot Password</a>
+          <span class="mx-2 text-gray-300">/</span>
+          <a className='text-blue-400 hover:text-blue-500' href='#'>Sign Up</a>
         </div>
       </form>
 
@@ -27,25 +28,3 @@ const Login = () => {
 
 
 export default Login;
-
-{/* <div>
-  <body class="flex flex-col items-center justify-center w-screen h-screen bg-gray-200 text-gray-700">
-
-
-  <h1 class="font-bold text-2xl">Welcome</h1>
-  <form class="flex flex-col bg-white rounded shadow-lg p-12 mt-12" action="">
-    <label class="font-semibold text-xs" for="usernameField">Username or Email</label>
-    <input class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text">
-    <label class="font-semibold text-xs mt-3" for="passwordField">Password</label>
-    <input class="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"type="password">
-    <button class="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">Login</button>
-    <div class="flex mt-6 justify-center text-xs">
-      <a class="text-blue-400 hover:text-blue-500" href="#">Forgot Password</a>
-      <span class="mx-2 text-gray-300">/</span>
-      <a class="text-blue-400 hover:text-blue-500" href="#">Sign Up</a>
-    </div>
-  </form>
-
-
-  </body>
-</div> */}
