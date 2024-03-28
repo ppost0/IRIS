a376e6c8706b858eaa3c258166b186e49400ffe0
ppost0 <p.post2321@yahoo.com>
2024-03-28 11:42:46 -0400

    Add user router and userController

---
+++ server/routes/userRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+const userController = require('../controllers/userController');
+
+router.post('/login', userController.login, (req, res) => {
+  return res.status(200).json('dummy response - login');
+});
+
+router.post('/signup', userController.signup, (req, res) => {
+  return res.status(200).json('dummy response - signup');
+});
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,29 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+const userRouter = require('./routes/userRouter.js');
 
-/*---------------------------------------------------------------*/
+/*==================================================================*/
 
 // Handle parsing of request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Handle requests for static files
 app.use(express.static(path.resolve(__dirname, '../client/public')));
 
 
-// GET Request to root
-app.get('/', (req, res) => {
-  res.send('Express Server');
-});
+// Define route handlers
+app.use('/user', userRouter);
 
 
 
@@ -40,7 +37,11 @@ app.get('/', (req, res) => {
 
 
 
-/*---------------------------------------------------------------*/
+
+
+
+/*=====================================================================*/
+
 // Catch-all route handler
 app.use('/*', (req, res) => {
   return res.status(404).send('Page not found - 404');
