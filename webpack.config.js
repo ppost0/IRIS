92792c63d80d847c58fd0e67f5ae797df94f382a
ppost0 <p.post2321@yahoo.com>
2024-04-01 15:36:31 -0400

    Revert TS and resolve path issues

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: process.env.NODE_ENV,
   entry: './client/src/index.js',
   output: {
-    path: path.resolve(__dirname, './client/public'),
+    path: path.resolve(__dirname, './client/dist'),
     filename: 'bundle.js'
   },
   devServer: {
