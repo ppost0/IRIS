04447608ab10168f000b525cef54ab20469163ce
ppost0 <p.post2321@yahoo.com>
2024-04-02 17:15:16 -0400

    Add userModel and controller functions

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,24 +1,73 @@
-const db = require('../models/userModel.js');
+const User = require('../models/userModel');
 
-const userController = {}
+const userController = {};
 
-userController.login = (req, res, next) => {
+/**
+* getAllUsers - retrieve all users from the database and stores it into res.locals
+* before moving on to next middleware.
+*/
+userController.getAllUsers = (req, res, next) => {
+  User.find({}, (err, users) => {
+    // if a database error occurs, call next with the error message passed in
+    // for the express global error handler to catch
+    if (err) return next('Error in userController.getAllUsers: ' + JSON.stringify(err));
+
+    // store retrieved users into res.locals and move on to next middleware
+    res.locals.users = users;
+    return next();
+  });
+};
+
+/**
+* createUser - create and save a new User into the database.
+*/
+userController.createUser = (req, res, next) => {
+
+  if (req.body.username && req.body.password) {
+    res.locals.newUser = {...req.body}; //Object.assign(req.body)
+    // insert thew newUser object the model document inside our mongoose
+    User.create(res.locals.newUser)
+      .then(
+        function() {
+          User.findOne({username: `${res.locals.user}`})
+            .then((data) => {
+              res.locals.id = data._id.toString();
+            })
+        }
+      )
+    return next();
+  } else {
+    return next('Error: Invalid user input');
+  }
+
+};
+
+/**
+* verifyUser - Obtain username and password from the request body, locate
+* the appropriate user in the database, and then authenticate the submitted password
+* against the password stored in the database.
+*/
+userController.verifyUser = (req, res, next) => {
+  // write code here
+  res.locals.user = req.body.username;
+  res.locals.pw = req.body.password;
 
 
+  User.findOne({username: `${res.locals.user}`})
+    .then((data) => {
+      console.log(data);
+      if (data !== null && data.password === res.locals.pw) {
+        res.locals.id = data._id.toString();
+        console.log(data._id.toString(), 'data id')
+        console.log(req.body, 'req.body');
+        return next();
+      }
+      else {
+        res.redirect('/signup');
+      }
+    });
+  // return next();
 
-}
-
-
-userController.signup = (req, res, next) => {
-
-
-
-}
-
-
-
-
-
-
+};
 
 module.exports = userController;
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,26 +1,12 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.uri;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
+module.exports = mongoose.model('User', userSchema);
