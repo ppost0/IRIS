d0ac68af76e04703873ee9f4ba3675677408119e
ppost0 <p.post2321@yahoo.com>
2024-04-04 15:20:44 -0400

    Add handling for existing usernames and emails

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,25 +5,28 @@ const userController = {};
 
 userController.signup = async (req, res, next) => {
 
-
   // Grab data from request here
   const { firstname, lastname, email, username, password } = req.body;
 
   // Check if required fields are missing
   if (!email, !username, !password) {
-    return res.status(400).json({ error: 'Required fields missing.' })
+    return res.status(400).json({ error: 'Required fields missing.' });
   }
 
+  // Check if email exists
+  const emailExists = await User.exists({ email });
+  console.log('emailexists --->', emailExists);
+  if (emailExists) {
+    return res.status(400).json({ error: 'Email already in use.' });
+  }
 
-  // dummy data for testing
-  // const account = {
-  //   firstname: 'Cynthia',
-  //   lastname: 'Jones',
-  //   username: 'cjones22',
-  //   email: 'hello@cynthia.com',
-  //   hashed_password: '6ca13d52ca70c883e0f0bb101e425a89e8624de51db2d2392593af6a84118090',
-  //   salt: 'salt',
-  // }
+  // Check if username exists
+  const usernameExists = await User.exists({ username });
+  console.log('usernamexists ---->', usernameExists)
+  if (usernameExists) {
+    return res.status(400).json({ error: 'Username already in use.' });
+  }
+
 
   // Add document to db
   try {
@@ -32,7 +35,7 @@ userController.signup = async (req, res, next) => {
     return next();
   }
   catch(error) {
-    return res.status(500).json('Could not register user ' + email );
+    return res.status(500).json({ error: error.message });
   };
 
 };
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -8,7 +8,7 @@ const userController = require('../controllers/userController');
 
 // Signup a new user
 router.post('/', userController.signup, (req, res) => {
-  return res.status(200).json('Registered user ' + res.locals.email);
+  return res.status(200).json('Registered user ' + res.locals.email + '.');
 });
 
 // Login an existing user
