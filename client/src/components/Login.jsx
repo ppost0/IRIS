def57f2b6c1ae45735ba32c751e54d8f37da2818
ppost0 <p.post2321@yahoo.com>
2024-04-30 14:38:59 -0400

    Connect login page frontend to backend login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,6 +3,35 @@ import logo from '../assets/images/1.png'
 
 const Login = () => {
 
+  const loginUser = async () => {
+    try {
+      // if (document.getElementById('passwordField').value === document.getElementById('reEnterPasswordField').value) {
+        const response = await fetch('/api/users/login', {
+          method: 'POST',
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: document.getElementById('usernameField').value.includes('@')
+            ? document.getElementById('usernameField').value : null,
+
+            username: document.getElementById('usernameField').value.includes('@')
+            ? null : document.getElementById('usernameField').value,
+
+            password: document.getElementById('passwordField').value
+          })
+        })
+
+
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+
+
+
   return (
 
     <div className='login-container flex flex-col items-center justify-center w-screen h-screen bg-neutral-900 text-stone-300'>
@@ -13,10 +42,10 @@ const Login = () => {
         </div>
         <form className='flex flex-col bg-stone-800 rounded-md shadow-lg p-12 mt-12' action=''>
           <label htmlFor='usernameField' className='font-semibold text-xs'>Username or Email</label>
-          <input className='flex items-center h-12 px-4 w-64 bg-neutral-700 mt-2 rounded focus:outline-none focus:ring-2' type='text'></input>
+          <input id='usernameField' className='flex items-center h-12 px-4 w-64 bg-neutral-700 mt-2 rounded focus:outline-none focus:ring-2' type='text'></input>
           <label htmlFor='passwordField' className='font-semibold text-xs mt-3'>Password</label>
-          <input className='flex items-center h-12 px-4 w-64 bg-neutral-700 mt-2 rounded focus:outline-none focus:ring-2' type='password'></input>
-          <button className='flex items-center justify-center h-12 px-6 w-64 bg-blue-500 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-600'>Login</button>
+          <input id='passwordField' className='flex items-center h-12 px-4 w-64 bg-neutral-700 mt-2 rounded focus:outline-none focus:ring-2' type='password'></input>
+          <button onClick={loginUser} type='button' className='flex items-center justify-center h-12 px-6 w-64 bg-blue-500 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-600'>Login</button>
           <div className='login-redirects flex mt-6 justify-center text-xs'>
             <a className='text-blue-500 hover:text-blue-600' href='#'>Forgot Password</a>
             <span id='login-logo' className="mx-2 text-neutral-700">/</span>
